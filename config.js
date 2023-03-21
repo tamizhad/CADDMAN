e326d413233827b6e4f751b4aa8c97952d68c06b
Tamil Selvan .B <tamil.selvan@zohocorp.com>
2023-03-21 12:19:39 +0530

    UI and feature update

--- config.js
+++ config.js
@@ -1,9 +1,24 @@
 const config = {
     db : {
         dbName : 'my_db',
-        userInfo : "user_info",
-        clientList: "client_list"   
-    }
+        userList : "user_list",
+        clientList: "client_list",
+        query: {
+            'add-client': 'INSERT INTO {{table}} VALUES("{{clientName}}", "{{clientContactPerson}}", "{{role}}", "{{clientContactNo}}", "{{email}}", "{{clientWebsite}}", "{{clientAddress}}", "{{clientState}}", "{{clientSales}}", "{{clientLeadStatus}}", "{{dueDate}}", "{{createdTime}}", "{{id}}");',
+
+            'add-user': 'INSERT INTO {{table}} VALUES("{{firstName}}", "{{lastName}}", "{{username}}", "{{password}}", "{{email}}", "{{role}}", "{{createdTime}}", "{{id}}");',
+            'get-user' : 'SELECT * FROM {{table}} WHERE username = "{{username}}";',            
+            
+            'get-rows': 'SELECT * FROM {{tableName}};',
+            'delete-row': 'DELETE FROM {{tableName}} WHERE id="{{curRowId}}";'
+        },
+        table: {
+            'add-client': 'clientList',
+
+            'add-user': 'userList',
+            'get-user': 'userList',
+        }
+    }    
 }
 
 //create table client_list(client_id varchar(255), client_name varchar(255), contact_person varchar(255), role varchar(255), contact_no varchar(255), email_id varchar(255), website_link varchar(255), address varchar(2000), state varchar(255), sales varchar(255), lead_status varchar(255), due_date varchar(255));
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cheerio = require('cheerio')
 const path = require('path')
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+const { v4: uuidv4 } = require('uuid');
 require('dotenv').config();
 const app = express()
 const port = process.env.APP_PORT;
@@ -37,26 +38,32 @@ app.get('/home', async function(req, res){
 
         var query = 'select * from '+config.db.clientList;
         var userObj = await executeQuery(query);
+
+        
         for(var i =0 ;i<userObj.results.length;i++){
-            var row =   '<tr id="client-list-'+(i+1)+'">'+
-                            '<td>'+(i+1)+'</td>'+
-                            '<td>'+(userObj.results[i].client_id)+'</td>'+
-                            '<td>'+(userObj.results[i].client_name)+'</td>'+
-                            '<td>'+(userObj.results[i].contact_person)+'</td>'+
-                            '<td>'+(userObj.results[i].role)+'</td>'+
-                            '<td>'+(userObj.results[i].contact_no)+'</td>'+
-                            '<td>'+(userObj.results[i].email_id)+'</td>'+
-                            '<td>'+(userObj.results[i].website_link)+'</td>'+
-                            '<td>'+(userObj.results[i].address)+'</td>'+
-                            '<td>'+(userObj.results[i].state)+'</td>'+
-                            '<td>'+(userObj.results[i].sales)+'</td>'+
-                            '<td>'+(userObj.results[i].lead_status)+'</td>'+
-                            '<td>'+(userObj.results[i].due_date)+'</td>'+
+            var row =   '<tr id="'+(userObj.results[i].id)+'">'+
+                            '<td class="data-sno">'+(i+1)+'</td>'+
+                            '<td class="data-clientId">'+(userObj.results[i].client_id)+'</td>'+
+                            '<td class="data-client-name">'+(userObj.results[i].client_name)+'</td>'+
+                            '<td class="data-client-contact-person">'+(userObj.results[i].contact_person)+'</td>'+
+                            '<td class="data-role">'+(userObj.results[i].role)+'</td>'+
+                            '<td class="data-contact-no">'+(userObj.results[i].contact_no)+'</td>'+
+                            '<td class="data-email">'+(userObj.results[i].email_id)+'</td>'+
+                            '<td class="data-client-website">'+(userObj.results[i].website_link)+'</td>'+
+                            '<td class="data-client-address">'+(userObj.results[i].address)+'</td>'+
+                            '<td class="data-client-state">'+(userObj.results[i].state)+'</td>'+
+                            '<td class="data-client-sales">'+(userObj.results[i].sales)+'</td>'+
+                            '<td class="data-client-lead-status">'+(userObj.results[i].lead_status)+'</td>'+
+                            '<td class="data-due-date" class="text-nowrap">'+(userObj.results[i].due_date)+'</td>'+
+                            '<td class="data-action" class="text-center"><span class="glyphicon glyphicon-edit edit" data-toggle="modal" data-target="#addClient"></span><span class="glyphicon glyphicon-trash delete" query-type="delete-row"></span></td>'+
                         '</tr>'
-            $('table#client-list').append(row);
+            $('table tbody').append(row);
         }
+
+
         res.send($.html());
     }catch(err){
+        console.log(err);
         res.sendFile(path.join(__dirname, 'public/internal-error.html'));
     }
 })
@@ -65,7 +72,7 @@ app.post('/authenticate', async function(req, res){
     try{
         var username = req.body.username;
         var password = req.body.password;
-        var query = 'select * from '+config.db.userInfo;
+        var query = 'select * from '+config.db.userList;
         var userObj = await executeQuery(query);
 
         for(user of userObj["results"]){
@@ -76,14 +83,52 @@ app.post('/authenticate', async function(req, res){
         }
         res.json({"code": 400, "error": "user not authenticated"}).end();
     }catch(err){
+        console.log(err);
         res.json({"code": 400, "error": "/authenticate - internal error"}).end();
+    }
+})
+
+app.post('/update-db', async function(req, res){
+    try{
+        var queryType = req.body.queryType;
+        
+        var query = config.db.query[queryType];
+        for(var key in req.body){
+            query = query.replace('{{'+key+'}}', req.body[key]);
+        }
+
+        if(req.body.dataType){
+            query = query.replace('{{tableName}}', req.body.dataType+"_list");    
+        }        
+
+        query = query.replace('{{table}}', config.db[config.db.table[queryType]]);
+        query = query.replace('{{id}}', uuidv4());
+        query = query.replace('{{createdTime}}', new Date());
+
+        if(/{{\w+}}/.test(query)){
+            res.json({"code": 400, "error": "value not replace properly"}).end();
+            return;
+        }
+        
+        var resultObj = await executeQuery(query);
+        if(resultObj && resultObj.status == 'success'){
+            res.json({"code": 200, "message": "db updated"}).end();
+            return;
+        }
+        if(queryType == 'add-user' && resultObj && resultObj.status == 'failed' && resultObj.error && resultObj.error.code == 'ER_DUP_ENTRY'){
+            res.json({"code": 400, "message": "duplicate username"}).end();
+            return;
+        }
+        res.json({"code": 400, "error": "db not updated"}).end();
+    }catch(err){
+        console.log(err);
+        res.json({"code": 400, "error": "/update-db - internal error"}).end();
     }
 })
 
 async function getDbConnection(req, res){   
     return new Promise(async function(resolve, reject){
         try{
-            console.log("getDbConnection");
             if(con && con.state == "authenticated"){
                 resolve(con);
             }else{
@@ -96,6 +141,7 @@ async function getDbConnection(req, res){
                 });
             }
         }catch(err){
+            console.log(err);
             resolve(null);
         }  
     })
@@ -107,13 +153,14 @@ async function executeQuery(queryStr){
             var dbCon = await getDbConnection();            
             await dbCon.query(queryStr, function(error, results, fields){
                 if (error){
-                    resolve(null);
+                    resolve({"error": error, "results": results, "fields": fields, "status": "failed"});
                 }else{
-                    resolve({"error": error, "results": results, "fields": fields});
+                    resolve({"error": error, "results": results, "fields": fields, "status": "success"});
                 }                
             });
         }catch(err){
-            resolve(null);
+            console.log(err);
+            resolve({"error": error, "results": results, "fields": fields, "status": "failed"});
         }
     });
 }
